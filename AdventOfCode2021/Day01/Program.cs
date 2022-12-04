e42eca725c10848b0994b83f528923ad5615095e
Graeme McIntosh <mr.g.mcintosh@gmail.com>
2022-12-04 15:14:42 +0200

    Adding tracked files

---
+++ AdventOfCode2021/Day01/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Day01;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        // 1. Read in the data
+        string[] tmpLines = File.ReadAllLines("data.txt");
+
+        // tmpLines = new[]
+        // {
+        //     "199",
+        //     "200",
+        //     "208",
+        //     "210",
+        //     "200",
+        //     "207",
+        //     "240",
+        //     "269",
+        //     "260",
+        //     "263"
+        // };
+        
+        int[] lines = new int[tmpLines.Length];
+
+        for (int i = 0; i < tmpLines.Length; i++)
+        {
+            if (!int.TryParse(tmpLines[i], out int depth))
+            {
+                Console.WriteLine("Invalid depth: '" + tmpLines[i] + "'.");
+                return;
+            }
+
+            lines[i] = depth;
+        }
+        
+        int numberOfTimesTheDepthIncreased = 0;
+
+        int previousDepth = lines[0];
+
+        // 2. Process the data for part 1.
+        for (int i = 1; i < lines.Length; i++)
+        {
+            int depth = lines[i];
+            if (depth > previousDepth)
+            {
+                numberOfTimesTheDepthIncreased++;
+            }
+
+            previousDepth = depth;
+        }
+
+        int numberOfTimesWindowTotalIncreased = 0;
+        int previousWindowTotal = lines[0] + lines[1] + lines[2];
+        // 3. Process the data for part 2
+        for (int i = 1; i < lines.Length-2; i++)
+        {
+            int windowTotal = lines[i] + lines[i + 1] + lines[i + 2];
+            if (windowTotal > previousWindowTotal)
+            {
+                numberOfTimesWindowTotalIncreased++;
+            }
+
+            previousWindowTotal = windowTotal;
+        }
+        
+        Console.WriteLine("Measurements that are larger: " + numberOfTimesTheDepthIncreased);
+        Console.WriteLine("Window totals that increased: " + numberOfTimesWindowTotalIncreased);
+    }
+}
---
+++ AdventOfCode2021/Day02/Program.cs
@@ -0,0 +1,96 @@
+﻿
+using Utilities;
+
+namespace Day02;
+
+public enum Command
+{
+    NotSet = 0,
+    forward,
+    down,
+    up
+}
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        // 1. Read the data in.
+        ValueTuple<Command, int>[] lines = FileUtils.ReadFile<Command>("data.txt");
+        
+        // 2. Process the data for part 1.
+        int horizontalPositionFromPart1 = 0;
+        int depthFromPart1 = 0;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            switch (lines[i].Item1)
+            {
+                case Command.forward:
+                {
+                    horizontalPositionFromPart1 += lines[i].Item2;
+                }
+                    break;
+
+                case Command.down:
+                {
+                    depthFromPart1 += lines[i].Item2;
+                }
+                    break;
+
+                case Command.up:
+                {
+                    depthFromPart1 -= lines[i].Item2;
+                }
+                    break;
+
+                case Command.NotSet:
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }            
+        }
+        
+        // 3. Part 2
+        int horizontalPositionFromPart2 = 0;
+        int depthFromPart2 = 0;
+        int aim = 0;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            switch (lines[i].Item1)
+            {
+                case Command.forward:
+                {
+                    int forwardMovement = lines[i].Item2;
+                    horizontalPositionFromPart2 += forwardMovement;
+                    depthFromPart2 += aim * forwardMovement;
+                }
+                    break;
+
+                case Command.down:
+                {
+                    aim += lines[i].Item2;
+                }
+                    break;
+
+                case Command.up:
+                {
+                    aim -= lines[i].Item2;
+                }
+                    break;
+
+                case Command.NotSet:
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }    
+        }
+
+        Console.WriteLine("Part 1");
+        Console.WriteLine("Horizontal position: " + horizontalPositionFromPart1);
+        Console.WriteLine("Depth: " + depthFromPart1);
+        Console.WriteLine("Horizontal position x depth: " + horizontalPositionFromPart1 * depthFromPart1);
+
+        Console.WriteLine("Part 2");
+        Console.WriteLine("Horizontal position: " + horizontalPositionFromPart2);
+        Console.WriteLine("Depth: " + depthFromPart2);
+        Console.WriteLine("Horizontal position x depth: " + horizontalPositionFromPart2 * depthFromPart2);
+    }
+}
