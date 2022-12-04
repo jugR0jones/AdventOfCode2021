aa1797e44a6181286f3854ee877ce8af7eaca57a
Graeme McIntosh <mr.g.mcintosh@gmail.com>
2022-12-04 14:49:27 +0200

    Added a utils class to help with loading data from files

---
+++ AdventOfCode2021/Day02/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Utilities;
+
+namespace Day02;
+
+public enum Command
+{
+    NotSet = 0,
+    forward,
+    down,
+    up
+}
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        // 1. Read the data in.
+//        string[] tmpLines = File.ReadAllLines("data.txt");
+
+         ValueTuple<Command, int>[] lines = FileUtils.ReadFile<Command>("data.txt");
+        
+        // 2. Process the data for part 1.
+        for (int i = 0; i < lines.Length; i++)
+        {
+            
+        }
+    }
+}
