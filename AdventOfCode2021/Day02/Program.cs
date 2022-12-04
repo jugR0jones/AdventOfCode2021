6a910fa334d2b3d1d113238dba7048cf83de3e0f
Graeme McIntosh <mr.g.mcintosh@gmail.com>
2022-12-04 14:54:08 +0200

    Day 2, Part 1

--- AdventOfCode2021/Day02/Program.cs
+++ AdventOfCode2021/Day02/Program.cs
@@ -21,9 +21,38 @@ internal static class Program
          ValueTuple<Command, int>[] lines = FileUtils.ReadFile<Command>("data.txt");
         
         // 2. Process the data for part 1.
+        int horizontalPosition = 0;
+        int depth = 0;
         for (int i = 0; i < lines.Length; i++)
         {
-            
+            switch (lines[i].Item1)
+            {
+                case Command.forward:
+                {
+                    horizontalPosition += lines[i].Item2;
+                }
+                    break;
+
+                case Command.down:
+                {
+                    depth += lines[i].Item2;
+                }
+                    break;
+
+                case Command.up:
+                {
+                    depth -= lines[i].Item2;
+                }
+                    break;
+
+                case Command.NotSet:
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }            
         }
+        
+        Console.WriteLine("Horizontal position: " + horizontalPosition);
+        Console.WriteLine("Depth: " + depth);
+        Console.WriteLine("Horizontal position x depth: " + horizontalPosition * depth);
     }
 }
