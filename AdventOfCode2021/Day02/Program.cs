ba62f2ab3390ee868fa320c2f08459170256fa18
Graeme McIntosh <mr.g.mcintosh@gmail.com>
2022-12-04 15:02:44 +0200

    Day 2, Part 2

--- AdventOfCode2021/Day02/Program.cs
+++ AdventOfCode2021/Day02/Program.cs
@@ -16,32 +16,30 @@ internal static class Program
     private static void Main(string[] args)
     {
         // 1. Read the data in.
-//        string[] tmpLines = File.ReadAllLines("data.txt");
-
-         ValueTuple<Command, int>[] lines = FileUtils.ReadFile<Command>("data.txt");
+        ValueTuple<Command, int>[] lines = FileUtils.ReadFile<Command>("data.txt");
         
         // 2. Process the data for part 1.
-        int horizontalPosition = 0;
-        int depth = 0;
+        int horizontalPositionFromPart1 = 0;
+        int depthFromPart1 = 0;
         for (int i = 0; i < lines.Length; i++)
         {
             switch (lines[i].Item1)
             {
                 case Command.forward:
                 {
-                    horizontalPosition += lines[i].Item2;
+                    horizontalPositionFromPart1 += lines[i].Item2;
                 }
                     break;
 
                 case Command.down:
                 {
-                    depth += lines[i].Item2;
+                    depthFromPart1 += lines[i].Item2;
                 }
                     break;
 
                 case Command.up:
                 {
-                    depth -= lines[i].Item2;
+                    depthFromPart1 -= lines[i].Item2;
                 }
                     break;
 
@@ -51,8 +49,48 @@ internal static class Program
             }            
         }
         
-        Console.WriteLine("Horizontal position: " + horizontalPosition);
-        Console.WriteLine("Depth: " + depth);
-        Console.WriteLine("Horizontal position x depth: " + horizontalPosition * depth);
+        // 3. Part 2
+        int horizontalPositionFromPart2 = 0;
+        int depthFromPart2 = 0;
+        int aim = 0;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            switch (lines[i].Item1)
+            {
+                case Command.forward:
+                {
+                    int forwardMovement = lines[i].Item2;
+                    horizontalPositionFromPart2 += forwardMovement;
+                    depthFromPart2 += aim * forwardMovement;
+                }
+                    break;
+
+                case Command.down:
+                {
+                    aim += lines[i].Item2;
+                }
+                    break;
+
+                case Command.up:
+                {
+                    aim -= lines[i].Item2;
+                }
+                    break;
+
+                case Command.NotSet:
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }    
+        }
+
+        Console.WriteLine("Part 1");
+        Console.WriteLine("Horizontal position: " + horizontalPositionFromPart1);
+        Console.WriteLine("Depth: " + depthFromPart1);
+        Console.WriteLine("Horizontal position x depth: " + horizontalPositionFromPart1 * depthFromPart1);
+
+        Console.WriteLine("Part 2");
+        Console.WriteLine("Horizontal position: " + horizontalPositionFromPart2);
+        Console.WriteLine("Depth: " + depthFromPart2);
+        Console.WriteLine("Horizontal position x depth: " + horizontalPositionFromPart2 * depthFromPart2);
     }
 }
